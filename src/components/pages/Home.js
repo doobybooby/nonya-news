c1478f8811b3cd73f0afad9d8bf356eaeb1e0aa1
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-08-12 00:52:40 -0400

    fetching ny times news

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 
 export const Home = () => {
+  const [collectiveNews, setCollectiveNews] = useState({})
 
-  console.log(process.env.REACT_APP_API_KEY)
+  const headerConfig = {
+    nytimesAPI: process.env.REACT_APP_API_KEY_NYTIMES,
+  }
   
+  const urls = {
+    nytimesURL: 'https://api.nytimes.com/svc/topstories/v2/home.json'
+  }
+
+  console.log(headerConfig, urls)
+
+  const fetchNYTimes = async()=> {
+    const response = await axios.get(urls.nytimesURL, { 
+      params: {
+        'api-key': process.env.REACT_APP_API_KEY_NYTIMES
+      }
+    })
+    setCollectiveNews(prevState => { return {...prevState, nyTimes:response.data.results}})
+  }
+  console.log(collectiveNews)
+
   return (
-    <div>Home
+    <div>
+      Home
+      <button onClick={fetchNYTimes}>NY-TIMSE</button>
+      <ul>
+        {
+          (collectiveNews.nyTimes || []).map( news => {
+            return (
+              <li>
+                {news.title}
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
