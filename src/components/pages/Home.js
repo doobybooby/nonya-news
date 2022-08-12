8eaace9ccc5a169cbffab46e48d78661bb018337
doobybooby <0933300+doobybooby@users.noreply.github.com>
2022-08-12 02:47:23 -0400

    4 news resources

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,37 +1,80 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import { useEffect } from 'react'
 
 
 export const Home = () => {
   const [collectiveNews, setCollectiveNews] = useState({})
-
-  const headerConfig = {
-    nytimesAPI: process.env.REACT_APP_API_KEY_NYTIMES,
-  }
   
   const urls = {
-    nytimesURL: 'https://api.nytimes.com/svc/topstories/v2/home.json'
+    nytimes: 'https://api.nytimes.com/svc/topstories/v2/home.json',
+    guardian: 'https://content.guardianapis.com/search?page=1',
+    mediastack: 'http://api.mediastack.com/v1/news'
+  }
+  
+  const fetchMediastack = async()=> {
+    const response = await axios.get(urls.mediastack, { 
+      params: {
+        'access_key': process.env.REACT_APP_API_KEY_MEDIASTACK,
+        'languages': 'en'
+      }    
+    })
+    setCollectiveNews(prevState => { return {...prevState, mediastack:response.data.data}})
   }
 
-  console.log(headerConfig, urls)
+  const fetchGuardian = async()=> {
+    const response = await axios.get(urls.guardian, { 
+      params: {
+        'api-key': process.env.REACT_APP_API_KEY_GUARDIAN
+      }
+      
+    })
+    setCollectiveNews(prevState => { return {...prevState, guardian:response.data.response.results}})
+  }
 
   const fetchNYTimes = async()=> {
-    const response = await axios.get(urls.nytimesURL, { 
+    const response = await axios.get(urls.nytimes, { 
       params: {
         'api-key': process.env.REACT_APP_API_KEY_NYTIMES
       }
     })
     setCollectiveNews(prevState => { return {...prevState, nyTimes:response.data.results}})
   }
-  console.log(collectiveNews)
+
+  useEffect(()=> {
+    const initialFetch = async()=> {
+      await Promise.all([
+        await fetchNYTimes(),
+        await fetchGuardian(),
+        await fetchMediastack()
+      ])
+    }
+    initialFetch()
+  }, [])
 
   return (
     <div>
-      Home
-      <button onClick={fetchNYTimes}>NY-TIMSE</button>
       <ul>
         {
           (collectiveNews.nyTimes || []).map( news => {
+            return (
+              <li>
+                {news.title}
+              </li>
+            )
+          })
+        }
+        {
+          (collectiveNews.guardian || []).map( news => {
+            return (
+              <li>
+                {news.webTitle}
+              </li>
+            )
+          })
+        }
+        {
+          (collectiveNews.mediastack || []).map( news => {
             return (
               <li>
                 {news.title}
